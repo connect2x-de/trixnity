4a51fa6f1ced01c48b7b6c62234267a39672d66c
Felix Hilgers <contact@fhilgers.com>
2026-08-17 00:49:13 +0200

    chore: update c2x-conventions to 20260917.190917

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -74,6 +74,6 @@ pluginManagement {
     }
 }
 
-plugins { id("de.connect2x.conventions.c2x-settings-plugin") version "20260828.073728" }
+plugins { id("de.connect2x.conventions.c2x-settings-plugin") version "20260917.190917" }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
