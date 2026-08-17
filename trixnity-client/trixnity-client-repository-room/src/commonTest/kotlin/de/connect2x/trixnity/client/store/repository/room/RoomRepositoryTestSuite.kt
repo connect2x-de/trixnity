9d49ea3b7756feb7aa26d6b11a24a43be48949ca
Felix Hilgers <contact@fhilgers.com>
2026-08-17 03:16:21 +0200

    test: create room database in common

--- trixnity-client/trixnity-client-repository-room/src/commonTest/kotlin/de/connect2x/trixnity/client/store/repository/room/RoomRepositoryTestSuite.kt
+++ trixnity-client/trixnity-client-repository-room/src/commonTest/kotlin/de/connect2x/trixnity/client/store/repository/room/RoomRepositoryTestSuite.kt
@@ -1,13 +1,14 @@
 package de.connect2x.trixnity.client.store.repository.room
 
-import androidx.room.RoomDatabase
+import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import de.connect2x.trixnity.client.RepositoriesModule
 import de.connect2x.trixnity.client.store.repository.test.RepositoryTestSuite
 
 class RoomRepositoryTestSuite :
     RepositoryTestSuite(
-        repositoriesModule = RepositoriesModule.room(randomDatabaseBuilder().apply { setDriver(BundledSQLiteDriver()) })
+        repositoriesModule =
+            RepositoriesModule.room(
+                Room.inMemoryDatabaseBuilder<TrixnityRoomDatabase>().setDriver(BundledSQLiteDriver())
+            )
     )
-
-expect fun randomDatabaseBuilder(): RoomDatabase.Builder<TrixnityRoomDatabase>
--- trixnity-client/trixnity-client-repository-room/src/jvmTest/kotlin/de/connect2x/trixnity/client/store/repository/room/RepositoryTestSuite.jvm.kt
+++
@@ -1,7 +0,0 @@
-package de.connect2x.trixnity.client.store.repository.room
-
-import androidx.room.Room
-import androidx.room.RoomDatabase
-
-actual fun randomDatabaseBuilder(): RoomDatabase.Builder<TrixnityRoomDatabase> =
-    Room.inMemoryDatabaseBuilder<TrixnityRoomDatabase>()
